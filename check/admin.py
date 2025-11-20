7e5b188b9bff5e96fc3dd12fe0b9c9b5dc27176e
Bagyshan <Bagishan01@gmail.com>
2025-11-20 13:09:58 +0600

    update admin panel

--- check/admin.py
+++ check/admin.py
@@ -5,10 +5,25 @@ from .models import Check
 # Register your models here.
 @admin.register(Check)
 class CheckAdmin(admin.ModelAdmin):
-    list_display = ('id', )
+    list_display = ('id', 'username', 'get_username_name', 'house_card', 'paid')
     autocomplete_fields = ['username', 'house_card', 'tariff']
 # Register your models here.
 
+    list_select_related = ['username', 'house_card']  # Оптимизация запросов
+    
+    def get_username_name(self, obj):
+        return obj.username.name if obj.username else '-'
+    
+    get_username_name.short_description = 'Имя пользователя'
+    get_username_name.admin_order_field = 'username__name'
+
+    search_fields = [
+        'house_card__house_card', 
+        'house_card__old_house_card', 
+        'username__email',
+        'username__name',
+        'house_card__address__street__name'
+    ]
 
 from .models import PaymentTransaction
 # Register your models here.
